056967edf87b24c30bcc1ab1b11a107133acf7c9
Mira ✌️ <miramarhaba@users.noreply.github.com>
2021-04-30 06:22:19 -0400

    Update conv2d.py

--- tflib/ops/conv2d.py
+++ tflib/ops/conv2d.py
@@ -106,9 +106,9 @@ def Conv2D(name, input_dim, output_dim, filter_size, inputs, he_init=True, mask_
         result = tf.nn.conv2d(
             input=inputs, 
             filter=filters, 
-            strides=[1, 1, stride, stride],
+            strides=[1, stride, stride, 1],
             padding='SAME',
-            data_format='NCHW'
+            data_format='NHWC'
         )
 
         if biases:
@@ -117,7 +117,7 @@ def Conv2D(name, input_dim, output_dim, filter_size, inputs, he_init=True, mask_
                 np.zeros(output_dim, dtype='float32')
             )
 
-            result = tf.nn.bias_add(result, _biases, data_format='NCHW')
+            result = tf.nn.bias_add(result, _biases, data_format='NHWC')
 
 
         return result
